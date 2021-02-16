f8aa9c29b7caf4d1fdcd2a536f5fff63567a394c
laturcotte <liamaturcotte@gmail.com>
2021-02-16 01:26:01 -0500

    started Welcome screen and Gauss-Jordan UI

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,4 +1,9 @@
 package main.java;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void handleMatrixDimensionsButton(ActionEvent actionEvent) {
+        System.out.println("Click");
+    }
 }
