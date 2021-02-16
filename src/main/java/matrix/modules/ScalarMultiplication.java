80ef5a632e1bc17b97f32c16ab494befbd57f26f
Sam B <bazinet.samuel1@gmail.com>
2021-02-15 22:06:42 -0500

    Added scalar multiplication class

--- src/main/java/matrix/modules/ScalarMultiplication.java
+++ src/main/java/matrix/modules/ScalarMultiplication.java
@@ -1,5 +1,5 @@
 /**
- * Transpose operator
+ * Scalar multiplication operator
  * 
  * Author: Samuel Bazinet
  * Last modification: February 15 2021
@@ -9,39 +9,41 @@ package main.java.matrix.modules;
 
 import main.java.matrix.Matrix;
 
-public class Transpose {
+public class ScalarMultiplication {
 
     public static void main(String[] args) {
 
         double[][] testMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
         Matrix matrix = new Matrix(testMatrix);
 
-        System.out.println("Pre-transpose:");
+        System.out.println("Pre-multiplication:");
         System.out.println(matrix);
-        System.out.println("Post-transpose");
-        System.out.println(transpose(matrix));
+        System.out.println("Post-multiplication * 5");
+        System.out.println(scalarMultiplication(matrix, 5));
+
     }
 
     /**
-     * Transposes a matrix
-     * @param matrix
-     * @return A transposed matrix
+     * Multiplies a matrix by a scalar 
+     * @param matrix 
+     * @param scalar
+     * @return A matrix multiplied by a scalar
      */
-    public static Matrix transpose(Matrix matrix) {
-
+    public static Matrix scalarMultiplication(Matrix matrix, int scalar) {
+        
         int numRows = matrix.getRowSize();
         int numCols = matrix.getColSize();
 
-        Matrix matrixOut = new Matrix(numCols, numRows, true);
+        Matrix matrixOut = new Matrix(numRows, numCols, true);
 
         for (int i = 0; i < numRows; i++) {
             for (int j = 0; j < numCols; j++) {
-                matrixOut.setEntry(j, i, matrix.getEntryAt(i, j));
+                matrixOut.setEntry(i, j, matrix.getEntryAt(i, j) * scalar);
             }
         }
 
         return matrixOut;
 
     }
-
+    
 }
